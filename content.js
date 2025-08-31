ea740607df010ef20c1a9e1f7fdfb7e7123ab928
lunarefract <51972322+lunarefract@users.noreply.github.com>
2025-08-31 21:32:14 +0200

    tests

--- content.js
+++ content.js
@@ -1,11 +1,17 @@
-function injectScript(filePath) {
+function injectScript(filePath, id) {
+    if (document.getElementById(id)) return;
+
     const script = document.createElement("script");
+    script.id = id;
+
     const runtime = chrome?.runtime || browser?.runtime;
     script.src = runtime.getURL(filePath);
-    script.onload = function() {
-        this.remove(); // cleanup
-    };
+
+    script.onload = () => script.remove();
+
+    console.debug("injecting");
     (document.head || document.documentElement).appendChild(script);
+    console.debug("injected");
 }
 
-injectScript("palitt.js");
+injectScript("palitt.js", "palitt-injected");
